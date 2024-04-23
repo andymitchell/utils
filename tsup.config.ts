b5bb11c848f3ed6fcca42254fd850220f47d413d
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2024-04-23 16:48:41 +0100

    Experimenting with making a module

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "tsup";
  
 export default defineConfig({
   entry: ["src/index.ts"],
-  publicDir: false,
   clean: true,
   minify: false,
   dts: true,
-  format: ['cjs', 'esm'], // When this changes, update 'type' in package.json 
+  target: 'node16',
+  format: ['cjs', 'esm', 'iife'], // When this changes, update 'type' in package.json 
 });
