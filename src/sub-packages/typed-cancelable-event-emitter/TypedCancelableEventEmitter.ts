f71de2a5dd1f9f9eb70afe1abfda3835fb63b674
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2024-08-01 11:25:49 +0100

    Update TypedCancellableEventEmitter with 'newListener' as a base

--- src/sub-packages/typed-cancelable-event-emitter/TypedCancelableEventEmitter.ts
+++ src/sub-packages/typed-cancelable-event-emitter/TypedCancelableEventEmitter.ts
@@ -4,9 +4,17 @@ import TypedEmitter, { EventMap } from "typed-emitter";
 
 // FYI could use eventemitter3 as isomorphic option, but its EventEmitter lacks setMaxListeners (because it has no limit), so would need TypedCancelableEventEmitter to implmenet the function as a placeholder, to satisfy TypedEmitter. 
 
-export type TypedCancel = () => void ;
-export class TypedCancelableEventEmitter<T extends EventMap> extends (EventEmitter as { new <U extends EventMap>(): TypedEmitter<U> })<T> {
-    onCancelable<E extends keyof T>(event: E, listener: T[E]): TypedCancel {
+interface BaseEventMap {
+    newListener: (eventName: string | symbol, listener: (...args: any[]) => void) => void;
+    removeListener: (eventName: string | symbol, listener: (...args: any[]) => void) => void;
+}
+type MergedEventMap<T extends EventMap> = T & BaseEventMap;
+
+
+
+export type TypedCancel = () => void;
+export class TypedCancelableEventEmitter<T extends EventMap> extends (EventEmitter as { new <U extends EventMap>(): TypedEmitter<U> })<MergedEventMap<T>> {
+    onCancelable<E extends keyof MergedEventMap<T>>(event: E, listener: MergedEventMap<T>[E]): TypedCancel {
         this.on(event, listener);
         return () => this.off(event, listener);
     }
@@ -19,29 +27,29 @@ export class TypedCancelableEventEmitter<T extends EventMap> extends (EventEmitt
      * @param errorOnTimeout 
      * @returns 
      */
-    onceConditionMet<E extends keyof T>(event: E, condition: (...args: Parameters<T[E]>) => boolean, timeoutMs = 30000, errorOnTimeout = false): Promise<{status: 'ok' | 'timeout', events: Parameters<T[E]>[]}> {
+    onceConditionMet<E extends keyof T>(event: E, condition: (...args: Parameters<T[E]>) => boolean, timeoutMs = 30000, errorOnTimeout = false): Promise<{ status: 'ok' | 'timeout', events: Parameters<T[E]>[] }> {
         return new Promise((resolve, reject) => {
-            const state:{cancel?:TypedCancel, timeout?: ReturnType<typeof setTimeout>, events: Parameters<T[E]>[]} = {events: []}
-            const wrappedListener = (...args:Parameters<T[E]>) => {
+            const state: { cancel?: TypedCancel, timeout?: ReturnType<typeof setTimeout>, events: Parameters<T[E]>[] } = { events: [] }
+            const wrappedListener = (...args: Parameters<T[E]>) => {
                 state.events.push(args);
-                if( condition(...args) ) {
+                if (condition(...args)) {
                     state.cancel!();
                     clearTimeout(state.timeout!);
-                    resolve({status: 'ok', events: state.events})
+                    resolve({ status: 'ok', events: state.events })
                 }
             };
-            state.cancel = this.onCancelable(event, wrappedListener as T[E]);
+            state.cancel = this.onCancelable(event, wrappedListener as MergedEventMap<T>[E]);
             state.timeout = setTimeout(() => {
                 state.cancel!();
-                
-                if( errorOnTimeout ) {
+
+                if (errorOnTimeout) {
                     reject(`onceConditionMet ${event.toString()} timeout. Did not achieve condition: ${condition.toString()}`)
                 } else {
-                    resolve({status: 'timeout', events: state.events});
+                    resolve({ status: 'timeout', events: state.events });
                 }
             }, timeoutMs);
         })
-        
+
     }
 
     /**
@@ -53,26 +61,26 @@ export class TypedCancelableEventEmitter<T extends EventMap> extends (EventEmitt
      * @param timeoutMs 
      * @returns 
      */
-    conditionMetAfterTimeout<E extends keyof T>(event: E, condition: (...args: Parameters<T[E]>) => boolean, timeoutMs = 30000): Promise<{status: 'ok' | 'fail', events: Parameters<T[E]>[]}> {
-        
+    conditionMetAfterTimeout<E extends keyof T>(event: E, condition: (...args: Parameters<T[E]>) => boolean, timeoutMs = 30000): Promise<{ status: 'ok' | 'fail', events: Parameters<T[E]>[] }> {
+
         return new Promise((resolve, reject) => {
-            const state:{cancel?:TypedCancel, timeout?: ReturnType<typeof setTimeout>, events: Parameters<T[E]>[], last_condition_met?: boolean} = {events: []}
-            const wrappedListener = (...args:Parameters<T[E]>) => {
+            const state: { cancel?: TypedCancel, timeout?: ReturnType<typeof setTimeout>, events: Parameters<T[E]>[], last_condition_met?: boolean } = { events: [] }
+            const wrappedListener = (...args: Parameters<T[E]>) => {
                 state.events.push(args);
                 state.last_condition_met = condition(...args);
             };
-            state.cancel = this.onCancelable(event, wrappedListener as T[E]);
+            state.cancel = this.onCancelable(event, wrappedListener as MergedEventMap<T>[E]);
             state.timeout = setTimeout(() => {
                 state.cancel!();
-                
-                if( state.last_condition_met ) {
-                    resolve({status: 'ok', events: state.events})
+
+                if (state.last_condition_met) {
+                    resolve({ status: 'ok', events: state.events })
                 } else {
-                    resolve({status: 'fail', events: state.events});
+                    resolve({ status: 'fail', events: state.events });
                 }
             }, timeoutMs);
         })
-        
+
     }
 }
 
