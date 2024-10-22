2fc4963ba50cf76c7f6c05be46776f7f3d3f0f59
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2024-10-22 13:13:24 +0100

    Improve help comment on applyPlaceholderToTemplateStringFunction

--- src/main/convert-placeholder-to-template-strings-array/applyPlaceholderToTemplateStringFunction.ts
+++ src/main/convert-placeholder-to-template-strings-array/applyPlaceholderToTemplateStringFunction.ts
@@ -1,13 +1,13 @@
 import convertPlaceholderToTemplateStringsArray from "./convertPlaceholderToTemplateStringsArray";
 
 /**
- * Apply a placeholder string and it's propertys to a template string function. 
+ * Apply a placeholder string and its parameters to a template string function. 
  * 
  *  
  * @param templateStringFunction The magic template string function, e.g. sql
  * @param placeholderString e.g. "SELECT * FROM users WHERE id = $1 AND updated_at > $2"
- * @param params The values, as an array, to replace in the placeholder (i.e. $1, $2)
- * @example if using Drizzle's sql function, which expects sql`SELECT * FROM ${table}`, you can call this instead to achieve the same with applyPlaceholderToTemplateStringFunction(sql, 'SELECT * FROM $1', [table])
+ * @param params The values, as an array, to replace in the placeholder (i.e. $1 / $2), e.g. [1, '2024-01-01']
+ * @example applyPlaceholderToTemplateStringFunction(sql, 'SELECT * FROM $1', [table]) // equivelent to calling drizzle's sql`SELECT * FROM ${table}`
  * 
  */
 export default function applyPlaceholderToTemplateStringFunction<T>(templateStringFunction:(templateStrings:TemplateStringsArray, ...params:any[]) => T, placeholderString: string, params:any[] ): T {
