bf6cefc2c7c69052cc090ac40daa7da100d44413
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2024-11-21 18:15:13 +0000

    Remove strip circular function - moving to objects

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,4 @@
-import {  PromiseWithTrigger, dLog, dLogDebug, dLogWarn, getGlobal, stripCircularReferences, midnight, promiseWithTrigger, sleep } from "./misc"
+import {  PromiseWithTrigger, dLog, dLogDebug, dLogWarn, getGlobal, midnight, promiseWithTrigger, sleep } from "./misc"
 
 
 import { ContentEditable } from "./content-editable"
@@ -19,8 +19,7 @@ export {
     dLog,
     dLogDebug,
     dLogWarn,
-    getGlobal,
-    stripCircularReferences
+    getGlobal
 }
 
 
