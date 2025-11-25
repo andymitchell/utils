42668dbc1d9d2c723dd89c4efd0cec3121600f13
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2025-11-25 15:37:56 +0000

    Export QueueConstructorOptions

--- src/sub-packages/queue/index-types.ts
+++ src/sub-packages/queue/index-types.ts
@@ -1,11 +1,12 @@
 
 import type { IQueueIo, QueueIoEvents } from "./common/helpers/item-queue/types.ts"
-import type { HaltPromise, IQueue, QueueFunction } from "./types.ts"
+import type { HaltPromise, IQueue, QueueConstructorOptions, QueueFunction } from "./types.ts"
 
 export type {
     IQueue,
     QueueFunction,
     IQueueIo,
     QueueIoEvents,
-    HaltPromise
+    HaltPromise,
+    QueueConstructorOptions
 }
